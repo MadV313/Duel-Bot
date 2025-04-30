80d113d1c526022bb3f60b8d5ff6f1c69c5494d9
MadV313 <snowvonia@gmail.com>
2025-04-30 12:14:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import duelRoutes from './routes/duel.js';
 import statusRoutes from './routes/status.js';
-import duelStartRoutes from './routes/duelStart.js'; // <- NEW
+import duelStartRoutes from './routes/duelStart.js';
+import summaryRoutes from './routes/duelSummary.js'; // <- NEW
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,7 +12,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/bot', duelRoutes);
-app.use('/duel', duelStartRoutes); // <- NEW
+app.use('/duel', duelStartRoutes);
+app.use('/summary', summaryRoutes); // <- NEW
 app.use('/', statusRoutes);
 
 app.get('/', (req, res) => {
