f4b7e6c9bf4e9569305fde5867a9e8efa70bbf13
MadV313 <snowvonia@gmail.com>
2025-04-30 17:52:12 -0400

    Update leave.js

--- commands/leave.js
+++ commands/leave.js
@@ -6,15 +6,21 @@ import { duelState } from '../logic/duelState.js';
 export default {
   data: new SlashCommandBuilder()
     .setName('leave')
-    .setDescription('Stop watching the duel'),
+    .setDescription('Leave the duel spectator view'),
 
   async execute(interaction) {
     const userId = interaction.user.id;
 
+    const before = duelState.spectators.length;
     duelState.spectators = duelState.spectators.filter(id => id !== userId);
+    const after = duelState.spectators.length;
+
+    const removed = before > after;
 
     return interaction.reply({
-      content: 'You have left the spectator view.',
+      content: removed
+        ? 'You have left the spectator view.'
+        : 'You were not watching the duel.',
       ephemeral: true
     });
   }
