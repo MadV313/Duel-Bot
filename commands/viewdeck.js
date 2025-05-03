58bd96f57b36f187ea39b34532eca08be6547a42
MadV313 <snowvonia@gmail.com>
2025-05-02 22:50:14 -0400

    Update viewdeck.js

--- commands/viewdeck.js
+++ commands/viewdeck.js
@@ -1,6 +1,8 @@
 // commands/viewdeck.js
 
 import { SlashCommandBuilder } from 'discord.js';
+import { isAllowedChannel } from '../utils/checkChannel.js';
+import config from '../config.json';
 
 export default {
   data: new SlashCommandBuilder()
@@ -8,6 +10,14 @@ export default {
     .setDescription('View your current deck in the Deck Builder UI'),
 
   async execute(interaction) {
+    // Restrict to #manage-cards
+    if (!isAllowedChannel(interaction.channelId, ['manageCards'])) {
+      return interaction.reply({
+        content: 'This command can only be used in #manage-cards.',
+        ephemeral: true
+      });
+    }
+
     const userId = interaction.user.id;
     const url = `https://your-username.github.io/Deck-Builder-UI/?user=${userId}`;
 
