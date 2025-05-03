93a85e6a4fd1aab09a36b6bf5233f1d00449080b
MadV313 <snowvonia@gmail.com>
2025-05-03 09:11:18 -0400

    Create index.js

---
+++ commands/index.js
@@ -0,0 +1,29 @@
+// commands/index.js
+
+import practice from './practice.js';
+import linkdeck from './linkdeck.js';
+import challenge from './challenge.js';
+import watch from './watch.js';
+import leave from './leave.js';
+import buycard from './buycard.js';
+import sellcard from './sellcard.js';
+import givecard from './givecard.js';
+import viewdeck from './viewdeck.js';
+import discard from './discard.js';
+import coin from './coin.js';
+import viewlog from './viewlog.js';
+
+export default [
+  practice,
+  linkdeck,
+  challenge,
+  watch,
+  leave,
+  buycard,
+  sellcard,
+  givecard,
+  viewdeck,
+  discard,
+  coin,
+  viewlog
+];
