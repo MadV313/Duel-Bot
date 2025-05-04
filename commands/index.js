71134b755df62f0ecee0c2135fe319c8b5518795
MadV313 <snowvonia@gmail.com>
2025-05-04 19:07:10 -0400

    Update index.js

--- commands/index.js
+++ commands/index.js
@@ -1,41 +1,45 @@
-import practice from './practice.js';
-import linkdeck from './linkdeck.js';
-import challenge from './challenge.js';
-import watch from './watch.js';
-import leave from './leave.js';
-import buycard from './buycard.js';
-import sellcard from './sellcard.js';
-import givecard from './givecard.js';
-import viewdeck from './viewdeck.js';
-import discard from './discard.js';
-import coin from './coin.js';
-import viewlog from './viewlog.js';
-import clear from './clear.js';
-import takecard from './takecard.js';
+// commands/index.js
+
 import accept from './accept.js';
+import buycard from './buycard.js';
+import challenge from './challenge.js';
+import clear from './clear.js';
+import coin from './coin.js';
 import deny from './deny.js';
-import save from './save.js';
+import discard from './discard.js';
 import forfeit from './forfeit.js';
+import givecard from './givecard.js';
+import leave from './leave.js';
+import linkdeck from './linkdeck.js';
+import practice from './practice.js';
+import rules from './rules.js';
+import save from './save.js';
+import sellcard from './sellcard.js';
+import takecard from './takecard.js';
 import victory from './victory.js';
+import viewdeck from './viewdeck.js';
+import viewlog from './viewlog.js';
+import watch from './watch.js';
 
 export default [
-  practice,
-  linkdeck,
-  challenge,
-  watch,
-  leave,
-  buycard,
-  sellcard,
-  givecard,
-  viewdeck,
-  discard,
-  coin,
-  viewlog,
-  clear,
-  takecard,
   accept,
+  buycard,
+  challenge,
+  clear,
+  coin,
   deny,
-  save,
+  discard,
   forfeit,
-  victory
+  givecard,
+  leave,
+  linkdeck,
+  practice,
+  rules,
+  save,
+  sellcard,
+  takecard,
+  victory,
+  viewdeck,
+  viewlog,
+  watch
 ];
