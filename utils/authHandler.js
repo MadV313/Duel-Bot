b04e160a162565c267831d949f4f31db3835d158
MadV313 <snowvonia@gmail.com>
2025-05-06 07:53:23 -0400

    Create authHandler.js

---
+++ utils/authHandler.js
@@ -0,0 +1,23 @@
+// authHandler.js
+
+import { isValidDiscordId } from './utils.js';
+import { config } from './config.json';
+
+/**
+ * Check if the user has the required admin permissions
+ * @param {string} userId - Discord ID of the user
+ * @returns {boolean} - True if the user is an admin
+ */
+export function isAdmin(userId) {
+  return config.adminIds.includes(userId);
+}
+
+/**
+ * Verifies if the player is eligible to join a duel
+ * @param {string} userId - Discord ID of the user
+ * @returns {boolean} - True if the player is eligible
+ */
+export function isEligibleForDuel(userId) {
+  // Logic to verify if the user is eligible (could be checking if they're in the right role, etc.)
+  return isValidDiscordId(userId);
+}
